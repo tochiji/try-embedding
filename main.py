a5cff38caeae1fbcf95ab2bd3aef11baeb1e2ebb
tochiji <7910558+tochiji@users.noreply.github.com>
2023-03-27 01:44:47 +0900

    データを外部のjsonに変更

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import json
 from sentence_transformers import SentenceTransformer, util
 
 def get_embed(sentence):
@@ -6,20 +7,12 @@ def get_embed(sentence):
 def get_cos_sim(embedding_1, embedding_2):
     return util.pytorch_cos_sim(embedding_1, embedding_2)  # type: ignore
 
-base_sentence = ["明石家さんまが会場で爆笑をさらった"]
-sentences = [
-    ["ビートたけしが舞台を盛り上げた"],
-    ["お笑い芸人は人を笑わせることが仕事だ"],
-    ["安倍晋三は日本の元内閣総理大臣です"],
-    ["松本人志はお笑いに関する映画を撮影した"],
-    ["イヌは散歩する"],
-    ["間寛平が一発ギャクを披露した"],
-    ["ジミー大西はよくお笑い番組に出演している"],
-    ["タモリのミュージックステーションは面白い"],
-    ["みずほ銀行は日本の金融機関だ"],
-    ["しょうゆうことという明石家さんまのギャグが存在する"],
-    ["杉本高文は明石家さんまの本名です"],
-]
+# JSONファイルからデータを読み込む
+with open("sentences.json", "r") as file:
+    data = json.load(file)
+
+base_sentence = data["base_sentence"]
+sentences = data["sentences"]
 
 model = SentenceTransformer("sentence-transformers/all-MiniLM-L6-v2")
 
@@ -30,5 +23,5 @@ embeddings = [get_embed(sentence) for sentence in sentences]
 # Compute and print similarities.
 for i, embed in enumerate(embeddings):
     similarity = get_cos_sim(base_embedding, embed)
-    print(f"{base_sentence} と {sentences[i]} の類似度:\t\t {similarity}")
+    print(f"{base_sentence} と {sentences[i]} の類似度:\t\t {similarity.item()}")
 
