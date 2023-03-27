3c2ad509d8f6a41a05961452f5e2eb8b4c0ff0ab
tochiji <7910558+tochiji@users.noreply.github.com>
2023-03-28 02:44:55 +0900

    類似度を順位付けできるように修正

--- main.py
+++ main.py
@@ -7,21 +7,36 @@ def get_embed(sentence):
 def get_cos_sim(embedding_1, embedding_2):
     return util.pytorch_cos_sim(embedding_1, embedding_2)  # type: ignore
 
+def check(data):
+    base_sentence = data["base_sentence"]
+    sentences = data["sentences"]
+
+    # Compute embedding for both lists
+    base_embedding = get_embed(base_sentence)
+    embeddings = [get_embed(sentence) for sentence in sentences]
+
+    result = []
+
+    # Compute and print similarities.
+    for i, embed in enumerate(embeddings):
+        similarity = get_cos_sim(base_embedding, embed)
+        result.append([sentences[i], similarity.item()])
+
+    # 類似度の高い順にソート
+    result.sort(key=lambda x: x[1], reverse=True)
+
+    # 結果を表示
+    print(f"基準文: {base_sentence}")
+    for i, res in enumerate(result):
+        print(f"{i+1}位: {res[0]} ({res[1]})")
+    
+
 # JSONファイルからデータを読み込む
 with open("sentences.json", "r") as file:
     data = json.load(file)
 
-base_sentence = data["base_sentence"]
-sentences = data["sentences"]
-
 model = SentenceTransformer("sentence-transformers/all-MiniLM-L6-v2")
 
-# Compute embedding for both lists
-base_embedding = get_embed(base_sentence)
-embeddings = [get_embed(sentence) for sentence in sentences]
-
-# Compute and print similarities.
-for i, embed in enumerate(embeddings):
-    similarity = get_cos_sim(base_embedding, embed)
-    print(f"{base_sentence} と {sentences[i]} の類似度:\t\t {similarity.item()}")
-
+for d in data:
+    check(d)
+    print()
