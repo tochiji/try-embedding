e70c998de78497ccbc8d2228722b7854cf213e74
tochiji <7910558+tochiji@users.noreply.github.com>
2023-03-31 01:56:11 +0900

    スクレイピングプログラムを修正

--- blog_scraper/src/bin/main.rs
+++ blog_scraper/src/bin/main.rs
@@ -1,25 +1,36 @@
-use reqwest::blocking::get;
+use reqwest::blocking::{get, Response};
 use select::document::Document;
 use select::predicate::Class;
 
 fn main() {
-    let url = "XXXX"; // ここにブログ記事のURLを入れてください
-    let response = get(url).expect("Failed to fetch the HTML");
+    match run() {
+        Ok(_) => (),
+        Err(e) => eprintln!("{}", e),
+    }
+}
 
-    let document = Document::from_read(response).expect("Failed to parse the HTML");
+fn run() -> Result<(), Box<dyn std::error::Error>> {
+    let page = 1;
+    let response = fetch_url(page)?;
+    let document = Document::from_read(response)?;
 
-    let title = document
-        .find(Class("post-ttl"))
-        .next()
-        .expect("Failed to find the title")
-        .text();
+    for post_box in document.find(Class("post-box")) {
+        if let Some(post_title) = post_box.find(Class("post-tit")).next() {
+            println!("Title: {}", post_title.text());
+        }
+        if let Some(post_date) = post_box.find(Class("post-date")).next() {
+            println!("Date: {}", post_date.text());
+        }
+        if let Some(url) = post_box.find(Class("more-link")).next() {
+            println!("URL: {}", url.attr("href").unwrap());
+        }
+        println!("---");
+    }
 
-    let content = document
-        .find(Class("post-body")) // ここに本文を格納している要素のクラス名を入れてください
-        .next()
-        .expect("Failed to find the content element")
-        .text();
+    Ok(())
+}
 
-    println!("Title: {}", title);
-    println!("Content: {}", content);
+fn fetch_url(page: i32) -> Result<Response, Box<dyn std::error::Error>> {
+    let url = format!("https://anagrams.jp/blog/page/{page}");
+    Ok(get(url)?)
 }
